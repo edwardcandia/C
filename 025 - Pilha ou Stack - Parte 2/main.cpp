b184b15e35624101b28fe113586dcf5a88379f7e
Edward Cândia <146676947+edwardcandia@users.noreply.github.com>
2024-04-26 16:30:35 -0300

    Add files via upload

---
+++ 025 - Pilha ou Stack - Parte 2/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    stack <string> rankArmy;
+    
+    rankArmy.push("Second Lieutenant");
+    rankArmy.push("First Lieutenant");
+    rankArmy.push("Captain");
+    rankArmy.push("Major");
+    rankArmy.push("Lieutenant Colonel");
+    rankArmy.push("Colonel");
+    rankArmy.push("Brigadier General");
+    rankArmy.push("Major General");
+    rankArmy.push("Lieutenant General");
+    rankArmy.push("General");
+    
+    std::cout << "Army Officer Ranks" << std::endl;
+
+    while (!rankArmy.empty()){
+        
+        std::cout << "Size of stack: "<< rankArmy.size() << std::endl;
+        
+        std::cout << "Top Army Officer: "<< rankArmy.top()<< std::endl;
+        
+        rankArmy.pop();
+    }
+    
+    if (rankArmy.empty()){
+
+        std::cout << "Empty Stack." << std::endl;
+        
+    }
+
+    
+    return 0;
+}
